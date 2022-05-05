fbd9cf2f99249a1f28e70b510181aa746592850a
josep <jopirulim@gmail.com>
2022-05-05 10:42:54 +0100

    Login a funcionar e com tentativa de envio de id

--- public/Dashboard/js/formSender.js
+++ public/Dashboard/js/formSender.js
@@ -16,7 +16,7 @@ async function upDate() {
 
         //get json here
         let newProduct = await $.ajax({
-            url: "https://cfg-api-ultimate.herokuapp.com/users",
+            url: "https://cfg-api-ultimate.herokuapp.com/users/"+id,
             method: "post",
             data: JSON.stringify(data),
             contentType: "application/json",
